07dee3c2d7e9b4d72361367dedd04734f47c1193
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-11-07 11:31:44 -0300

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
           get 'by_date/:date', to: 'ufs#by_date'
         end
       end
+      resources :clients, only: [] do
+        collection do
+          get ':name', to: 'clients#by_name'
+        end
+      end
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
